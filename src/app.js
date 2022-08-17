04e0ec2702f6609d54c79dc9ae7d557171d8d4d6
Slntcoda <ask.sunn@live.com>
2022-08-17 13:29:57 +0100

    Setup the function to call data from API based on the page index and generate dynamic table

--- src/app.js
+++ src/app.js
@@ -1,8 +1,38 @@
 const nextBtn = document.getElementById('nextBtn')
 const prevBtn = document.getElementById('prevBtn')
+const data = document.getElementById('data')
+const getPage = data.getAttribute('data-page')
+const pageNo = document.getElementById('page-info')
 
+function fetchData(index) {
+  fetch(
+    'https://randomapi.com/api/8csrgnjw?key=LEIX-GF3O-AG7I-6J84&page=' + index
+  )
+    .then((response) => response.json())
+    .then((json) => {
+      const holdData = json.results[0][index]
 
+      // create table row and input table data from json data collected from randomapi.com
+      data.innerHTML = ''
+      for (let h = 0; h < holdData.length; h++) {
+        data.innerHTML +=
+          '<tr>' +
+          '<td>' +
+          holdData[h].row +
+          '</td>' +
+          '<td>' +
+          holdData[h].gender +
+          '</td>' +
+          '<td>' +
+          holdData[h].age +
+          '</td>' +
+          '</tr>'
+        // console.log(holdData[h].age);
+      }
 
-const startApp = async () => {}
-
-document.addEventListener('DOMContentLoaded', startApp)
+      //set page attribut to current page number
+      data.setAttribute('data-page', index)
+      pageNo.innerHTML = index
+    })
+}
+fetchData(getPage)
