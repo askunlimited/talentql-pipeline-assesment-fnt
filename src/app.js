19d120a0ba92bd5009b57a1b0fe0aec62883e4b6
Slntcoda <ask.sunn@live.com>
2022-08-17 11:33:54 +0100

    Initial setup of the index and app.js files

---
+++ src/app.js
@@ -0,0 +1,8 @@
+const nextBtn = document.getElementById('nextBtn')
+const prevBtn = document.getElementById('prevBtn')
+
+
+
+const startApp = async () => {}
+
+document.addEventListener('DOMContentLoaded', startApp)
